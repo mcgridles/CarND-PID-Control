8367488a4b3cfc7b89951f0e45d796e204d6df23
mcgridles <henry.gridley@gmail.com>
2018-11-09 17:46:28 -0500

    feat: Complete PID class and project
    This commit completes the PID class which is essentially the entire project. The initial tau values were found by tweaking the values given in the lesson. The PID class is a simple implementation of a PID controller which updates the P, I, and D errors based on the current crosstrack error. This error is then used to calculate the new steering angle value.

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,3 +1,4 @@
+#include <limits>
 #include "PID.h"
 
 using namespace std;
@@ -11,11 +12,24 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+
+    p_error = std::numeric_limits<double>::max();
 }
 
 void PID::UpdateError(double cte) {
+    if (p_error == std::numeric_limits<double>::max()) {
+        p_error = cte;
+    }
+
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
 }
 
 double PID::TotalError() {
+    return Kp * p_error + Ki * i_error + Kd * d_error;
 }
 
