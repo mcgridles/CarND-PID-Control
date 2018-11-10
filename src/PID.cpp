135d8e4dc9c52a71573b370e2239a111735907dc
mcgridles <henry.gridley@gmail.com>
2018-11-10 15:26:53 -0500

    feat: Complete reflection write-up
    This commit adds the reflection write-up that was forgotten originally. It explains the values chosen for the P, I, and D terms of the controller and how they were decided upon.

--- src/PID.cpp
+++ src/PID.cpp
@@ -4,8 +4,46 @@
 using namespace std;
 
 /*
-* TODO: Complete the PID class.
-*/
+ * ****************** Reflection ******************
+ * Proportional term: 0.12
+ *      This term controls how hard the car steers
+ *      towards the goal line. A value of 0.12 is
+ *      a good sweet-spot because it does not turn
+ *      too quickly which causes an erratic driving
+ *      pattern, but the car also does not end up
+ *      too far off of the goal line.
+ *
+ *      I am also able to have a lower value in this
+ *      case because the car starts in the center of
+ *      the lane so the CTE is very small. If the car
+ *      started too far away from the center, a small
+ *      P term might cause the car to take a long time
+ *      to correct itself and it could drive off the
+ *      road in that time.
+ *
+ * Integral term: 0.0
+ *      This term controls how quickly the car corrects
+ *      for systematic bias. Because the car is simulated
+ *      and seems to be programmed to not have any
+ *      systematic bias (it drives perfectly straight when
+ *      the driving angle is 0 degrees), this term can
+ *      be set to 0.0.
+ *
+ * Differential term: 2.5
+ *      This term controls the amount the car counter-steers
+ *      as it gets closer to the goal line. A value of 2.5
+ *      reduces oscillations in the motion of the car as it
+ *      tries to correct itself, but doesn't cause the car to
+ *      steer too slowly towards the line.
+ *
+ * How coefficients were chosen:
+ *      All three values were chosen by taking values given
+ *      in the lessons and manually tweaking them until they
+ *      produced good results. The original values already
+ *      caused the controller to perform decently, so this
+ *      didn't take too long.
+ * ************************************************
+ */
 
 PID::PID() {}
 
--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,6 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  // TODO: Initialize the pid variable.
   // Values found by tweaking lesson values
   pid.Init(0.12, 0.0, 2.5);
 
@@ -53,12 +52,7 @@ int main()
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value = 0.0;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
+
           pid.UpdateError(cte);
 
           steer_value -= pid.TotalError();
